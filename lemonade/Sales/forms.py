7e124f1fc47d40c37db819cc204ec11385ed3340
Flamebrawler <flamebrawler123@gmail.com>
2020-05-19 15:06:22 -0400

    small changes

--- lemonade/Sales/forms.py
+++ lemonade/Sales/forms.py
@@ -2,22 +2,6 @@ from django import forms
 from .models import Staff, Lemonade
 
 
-months = (
-    (1, 'Jan'),
-    (2, 'Feb'),
-    (3, 'Mar'),
-    (4, 'Apr'),
-    (5, 'May'),
-    (6, 'Jun'),
-    (7, 'Jul'),
-    (8, 'Aug'),
-    (9, 'Sep'),
-    (10, 'Oct'),
-    (11, 'Nov'),
-    (12, 'Dec'),
-)
-
-
 def iterable(queryset, defaulttext='Select Option'):
     return (('', defaulttext),)+tuple(zip(range(1, len(queryset)+1), queryset))
 
